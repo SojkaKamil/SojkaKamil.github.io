ed0342253cd9dc380c1e647c1229ebc2cf4a9bd5
SojkaKamil <63636587+SojkaKamil@users.noreply.github.com>
2020-04-27 16:56:58 +0200

    Create script.js

---
+++ Events & Event Listeners/script.js
@@ -0,0 +1,24 @@
+$("button").on('click', (event) => {
+    console.log("Yeah, you clicked me");
+  });
+
+  $("#first").click(() => {
+    $("#first").text("Yeah, you clicked me.");
+  });
+
+  $("#second").click(() => {
+    $("#first").text("Second one was clicked.");
+    $("#second").text("Yeah, you clicked me.");
+  });
+
+
+$("#prettify").click(() => {
+  $("#prettify").text("Yeah, you clicked me.");
+  $("#second").text("Third one was clicked and colored all.");
+  $("button").css("background", "violet");
+  $("button").css("color", "white");
+  $("button").css("font-size", "20px");
+  $("button").css("border", "0");
+  $("button").css("border-radius", "5px");
+})
+  
